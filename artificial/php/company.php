56ad0575eaf9ca387a744c0524b68e6321b57304
Dayan Balevski <borovez@hotmail.com>
2015-05-13 19:05:25 +0300

    ooops

---
+++ artificial/php/company.php
@@ -0,0 +1,15 @@
+<section id="company" class="hdr-sc">
+  <div class="container">
+  </div>
+  <section class="img-hdr">
+    <div class="container">
+      <h2>COMPANY</h2>
+    </div>
+    <img src="/artificial/images/abstract-1.jpg" style="height:1080px; margin-top:-34%;"></img>
+  </section>
+  <section class="med-grey-sc rg-sc">
+    <div class="container">
+      content
+    </div>
+  </section>
+</section>
