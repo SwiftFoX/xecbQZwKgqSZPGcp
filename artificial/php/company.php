85025875e265c2700d9e80b1da9deeb62939a41a
Dayan Balevski <borovez@hotmail.com>
2015-05-16 01:17:08 +0300

    almost done company page

--- artificial/php/company.php
+++ artificial/php/company.php
@@ -1,13 +1,21 @@
 <link rel="stylesheet" href="/artificial/css/company.css">
 <script src="/artificial/js/jquery.circlemouse.js"></script>
 <section id="company" class="hdr-sc">
-  <div class="container">
-  </div>
   <section class="img-hdr">
     <div class="container">
       <h2>COMPANY</h2>
     </div>
     <img src="/artificial/images/abstract-1.jpg" style="height:1080px; margin-top:-34%;"></img>
+  </section>
+  <section class="med-lt-grey-sc rg-sc title-sc">
+    <div class="container quote">
+      <p>Artificial Animation is a <strong>flexible and cutting edge IT service provider</strong>
+        based in Zurich. We offer your company <strong>tailored solutions</strong> as well as
+        <strong>competent consulting</strong> in many fields. The main focus of Syonix lies on
+        <strong>web and individual development</strong> as well as management and processing of
+        business and customer data.
+      </p>
+    </div>
   </section>
   <section class="lt-grey-sc rg-sc title-sc">
     <header class="header-divider">
@@ -15,25 +23,12 @@
     </header>
     <div class="arrow-sc"></div>
   </section>
-  <section class="med-lt-grey-sc rg-sc">
+  <section class="med-lt-grey-sc rg-sc team-sc">
     <div class="container">
       <section id="team">
         <div class="member">
-          <div class="member-circle">
-            <img src="/artificial/images/me.jpg" alt="Dayan Balevski - Graphic Designer &amp; Web Developer">
-            <div class="inner-circle"></div>
-            <div class="social-circles">
-              <a class="soc-circle fb-circle" href="https://www.facebook.com/dayan.balevski"
-              data-container="body" data-toggle="popover" data-placement="right" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus."><i class="fa fa-facebook"></i></a>
-              <a class="soc-circle tw-circle" href="https://twitter.com/Dayan_Balevski"><i class="fa fa-twitter"></i></a>
-              <a class="soc-circle li-circle" href="https://www.linkedin.com/pub/dayan-balevski/82/112/96"><i class="fa fa-linkedin"></i></a>
-              <a class="soc-circle gp-circle" href="https://plus.google.com/+DayanBalevski/"><i class="fa fa-google-plus"></i></a>
-            </div>
-          </div>
           <strong>Dayan Balevski</strong>
         	<p>Graphic Designer &amp; Web Developer</p>
-        </div>
-        <div class="member">
           <div class="member-circle">
             <img src="/artificial/images/me.jpg" alt="Dayan Balevski - Graphic Designer &amp; Web Developer">
             <div class="inner-circle"></div>
@@ -45,11 +40,15 @@
               <a class="soc-circle gp-circle" href="https://plus.google.com/+DayanBalevski/"><i class="fa fa-google-plus"></i></a>
             </div>
           </div>
-          <strong>Ronald Griffin</strong>
-        	<p>Motion Graphics Designer</p>
+          <p class="member-description">Currently studying Computer Science­ with focus on software
+            engineering,­ user interaction­ and information security.­ Over 6 years
+            of experience in web- and­ application development.
+          </p>
         </div>
         <div class="member">
-          <div class="member-circle ">
+          <strong>Ronald Griffin</strong>
+        	<p>Motion Graphics Designer</p>
+          <div class="member-circle">
             <img src="/artificial/images/me.jpg" alt="Dayan Balevski - Graphic Designer &amp; Web Developer">
             <div class="inner-circle"></div>
             <div class="social-circles">
@@ -60,8 +59,73 @@
               <a class="soc-circle gp-circle" href="https://plus.google.com/+DayanBalevski/"><i class="fa fa-google-plus"></i></a>
             </div>
           </div>
-          <strong>Dan Lond</strong>
-        	<em>Writer</em>
+          <p class="member-description">Currently studying Computer Science­ with focus on software
+            engineering,­ user interaction­ and information security.­ Over 6 years
+            of experience in web- and­ application development.
+          </p>
+        </div>
+        <div class="member">
+          <strong>John Rowley</strong>
+        	<p>System Administrator</p>
+          <div class="member-circle">
+            <img src="/artificial/images/me.jpg" alt="Dayan Balevski - Graphic Designer &amp; Web Developer">
+            <div class="inner-circle"></div>
+            <div class="social-circles">
+              <a class="soc-circle fb-circle" href="https://www.facebook.com/dayan.balevski"
+              data-container="body" data-toggle="popover" data-placement="right" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus."><i class="fa fa-facebook"></i></a>
+              <a class="soc-circle tw-circle" href="https://twitter.com/Dayan_Balevski"><i class="fa fa-twitter"></i></a>
+              <a class="soc-circle li-circle" href="https://www.linkedin.com/pub/dayan-balevski/82/112/96"><i class="fa fa-linkedin"></i></a>
+              <a class="soc-circle gp-circle" href="https://plus.google.com/+DayanBalevski/"><i class="fa fa-google-plus"></i></a>
+            </div>
+          </div>
+          <p class="member-description quote">Currently studying Computer Science­ with focus on software
+            engineering,­ user interaction­ and information security.­ Over 6 years
+            of experience in web- and­ application development.
+          </p>
+        </div>
+      </section>
+    </div>
+  </section>
+  <section class="lt-grey-sc rg-sc title-sc">
+    <header class="header-divider">
+      <h2>Featured Clients.</h2>
+    </header>
+    <div class="arrow-sc"></div>
+  </section>
+  <section class="med-lt-grey-sc rg-sc">
+    <div class="container">
+      <section id="clients">
+        <div class="featured-row clearfix">
+          <div class="featured-col">
+            <a href="#" target="_blank">
+              <img class="logo" src="http://syonix.ch/en/wp-content/themes/syonix/img/clients/restlezz.png"/>
+            </a>
+          </div>
+          <div class="featured-col">
+            <a href="#" target="_blank">
+              <img class="logo" src="http://syonix.ch/en/wp-content/themes/syonix/img/clients/restlezz.png"/>
+            </a>
+          </div>
+          <div class="featured-col">
+            <a href="#" target="_blank">
+              <img class="logo" src="http://syonix.ch/en/wp-content/themes/syonix/img/clients/restlezz.png"/>
+            </a>
+          </div>
+          <div class="featured-col">
+            <a href="#" target="_blank">
+              <img class="logo" src="http://syonix.ch/en/wp-content/themes/syonix/img/clients/restlezz.png"/>
+            </a>
+          </div>
+          <div class="featured-col">
+            <a href="#" target="_blank">
+              <img class="logo" src="http://syonix.ch/en/wp-content/themes/syonix/img/clients/restlezz.png"/>
+            </a>
+          </div>
+          <div class="featured-col">
+            <a href="#" target="_blank">
+              <img class="logo" src="http://syonix.ch/en/wp-content/themes/syonix/img/clients/restlezz.png"/>
+            </a>
+          </div>
         </div>
       </section>
     </div>
